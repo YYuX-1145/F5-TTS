c4f6553a7818b5991858440a616829a22fd0b391
YYuX-1145 <you@example.com>
2025-03-14 13:41:42 +0800

    *

--- install_accelerate.bat
+++ install_accelerate.bat
@@ -3,6 +3,6 @@ set PYTHONPATH=%CD%\src
 set PATH=%PATH%;%CD%\ffmpeg\bin\;%CD%\py310\;%CD%\py310\Scripts
 set HF_ENDPOINT=https://hf-mirror.com
 set HF_HOME=%CD%\download\huggingface
-
-py310\python.exe src\f5_tts\train\finetune_gradio.py
+py310\python.exe -m pip uninstall -y accelerate
+py310\python.exe -m pip install accelerate==1.5.1
 pause
